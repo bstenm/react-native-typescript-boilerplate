0ccd625bc3b7d07887bb66da7136de42ccc93e70
Bertrand Steinmetz <bstenm@hotmail.com>
2019-03-11 16:25:37 -0500

    adding tslint rules, tsconfig and enzyme adapter for react16

--- App.tsx
+++ App.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-interface iHomeProps {};
-interface iHomeState {};
-
-export default class App extends React.Component<iHomeProps, iHomeState> {
+export default class App extends React.Component<{}, {}> {
       render() {
             return (
                   <View style={styles.container}>
@@ -16,9 +13,9 @@ export default class App extends React.Component<iHomeProps, iHomeState> {
 
 const styles = StyleSheet.create({
       container: {
-            flex: 1,
-            backgroundColor: '#fff',
             alignItems: 'center',
+            backgroundColor: '#fff',
+            flex: 1,
             justifyContent: 'center',
       },
 });
--- __tests__/exampleTest.tsx
+++ __tests__/exampleTest.tsx
@@ -1,9 +1,11 @@
 import { shallow } from 'enzyme';
 import React from 'react';
+import { Text } from 'react-native';
 import App from '../App';
 
 const wrapped = shallow(<App />);
 
 it('works', () => {
-      expect(wrapped.filter(Text).length).toBe(1);
+      expect(wrapped.find(Text).length).toBe(1);
+      expect(wrapped.find(Text).childAt(0).text()).toBe("Welcome!");
     });
